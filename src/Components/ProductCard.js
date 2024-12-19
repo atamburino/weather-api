fde99c56fec19992699b5fd1f5a607f2fcd60389
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2024-12-18 23:06:21 -0500

    product card styling

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 
 function ProductCard({ product: { name, price, description, companyName } }) {
     return (
-        <div>
-            <p>{name}</p>
-            <p>{description}</p>
-            <p>{companyName}</p>
+        <div className="product-card">
+            <h2 className="product-name">{name}</h2>
+            <p className="product-price">${price}</p>
+            <p className="product-description">{description}</p>
+            <p className="product-company">{companyName}</p>
         </div>
     )
 };
