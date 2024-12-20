65f0eebc52d384ec4fd2ca7d1c7ffd42fbffa00f
Andrew Tamburino <57277787+atamburino@users.noreply.github.com>
2024-12-19 22:34:27 -0500

    Fixed $ to only render when price is avaliable

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -5,7 +5,9 @@ function ProductCard({ product: { name, price, description, companyName } }) {
     return (
         <div className="product-card">
             <h2 className="product-name">{name}</h2>
-            <p className="product-price">${price}</p>
+            {/* Check if the price is available (not null or undefined). 
+            If it is, render the price with a dollar sign. If not, render an empty string */}
+            <p className="product-price">{price ? `$${price}` : ''}</p>
             <p className="product-description">{description}</p>
             <p className="product-company">{companyName}</p>
         </div>
